9298f6d2e32647677bcfb84c437c140098b07441
Parth Shukla <31998380+ParthShuklaa@users.noreply.github.com>
2021-11-11 12:28:35 +0530

    Add files via upload

---
+++ IMyService2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMyService2" in both code and config file together.
+    [ServiceContract]
+    public interface IMyService2
+    {
+        [OperationContract]
+        void DoWork();
+        [OperationContract]
+        List<UserDetail> GetAllUser();
+        [OperationContract]
+        UserDetail GetAllUserById(int id);
+        [OperationContract]
+        int AddUser(string Name, string Email);
+        [OperationContract]
+        int DeleteUserById(int Id);
+
+    }
+    [DataContract]
+    public class UserDetails
+    {
+        [DataMember]
+        public int Id { get; set;}
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ MyService2.svc.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+using System.Data.Entity;
+
+namespace MyWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MyService2" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MyService2.svc or MyService2.svc.cs at the Solution Explorer and start debugging.
+    public class MyService2 : IMyService2
+    {
+        public void DoWork()
+        {
+        }
+
+        public List<UserDetail> GetAllUser()
+        {
+            List<UserDetail> userlst = new List<UserDetail>();
+            MyDBEntities tstDb = new MyDBEntities();
+                var lstUsr = from K in tstDb.UserDetails select K;
+            foreach ( var item in lstUsr)
+            {
+                UserDetail usr = new UserDetail();
+                usr.ID = item.ID;
+                usr.Name = item.Name;
+                usr.Email = item.Email;
+                userlst.Add(usr);
+
+            }
+
+            return userlst;
+        }
+        public UserDetail GetAllUserById(int id)
+        {
+            MyDBEntities tstDb = new MyDBEntities();
+            var lstUsr = from k in tstDb.UserDetails where k.ID == id select k;
+            UserDetail usr = new UserDetail();
+            foreach (var item in lstUsr)
+            {
+                usr.ID = item.ID;
+                usr.Name = item.Name;
+                usr.Email = item.Email;
+            }
+            return usr;
+        }
+
+        public int DeleteUserById(int id)
+        {
+            MyDBEntities tstDb = new MyDBEntities();
+            UserDetail usrdtl = new UserDetail();
+            usrdtl.ID = id;
+            tstDb.Entry(usrdtl).State = EntityState.Deleted;
+            int Retval = tstDb.SaveChanges();
+            return Retval;
+        }
+
+        public int AddUser(string Name , string Email)
+        {
+            MyDBEntities tstDb = new MyDBEntities();
+            UserDetail usrdtl = new UserDetail();
+            usrdtl.Name = Name;
+            usrdtl.Email = Email;
+            tstDb.UserDetails.Add(usrdtl);
+            int retval = tstDb.SaveChanges();
+            return retval;
+        }
+    }
+}
